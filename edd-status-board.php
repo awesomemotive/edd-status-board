80edba0827a5541fb10279e95d77d67820046d99
Chris Christoff <chriscct7@users.noreply.github.com>
2015-07-30 00:49:49 -0400

    Spelling fix
    Make sure to rerun translations after merging this

--- edd-status-board.php
+++ edd-status-board.php
@@ -268,7 +268,7 @@ function edd_statusboard_profile_endpoint_display( $user ) {
 			<tbody>
 				<tr id="edd-sb-user-urls">
 					<th>
-						<label for="edd_set_api_key"><?php echo sprintf( __( 'Click a button to add the graph to Status Board. Requires an iPad with the <a href="%s" target="_blank">Status Board app</a> insatlled', 'edd-statusboard-txt' ), 'https://itunes.apple.com/us/app/status-board/id449955536?mt=8' ); ?></label>
+						<label for="edd_set_api_key"><?php echo sprintf( __( 'Click a button to add the graph to Status Board. Requires an iPad with the <a href="%s" target="_blank">Status Board app</a> installed', 'edd-statusboard-txt' ), 'https://itunes.apple.com/us/app/status-board/id449955536?mt=8' ); ?></label>
 					</th>
 					<td>
 						<?php if ( ( isset( $edd_options['api_allow_user_keys'] ) || current_user_can( 'manage_shop_settings' ) ) && current_user_can( 'view_shop_reports' ) ) : ?>
